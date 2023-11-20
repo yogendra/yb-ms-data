d76e554395e120cb78048ac0c8349fe7f0a889bb
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-11-20 06:08:56 +0000

    uncomment rest controller annotation

--- springboot/src/main/java/io/mservice/todo/TodoController.java
+++ springboot/src/main/java/io/mservice/todo/TodoController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
-// @RestController
-// @RequestMapping("/v1/todo")
+@RestController
+@RequestMapping("/v1/todo")
 public class TodoController {
 
 	private final ITodoService todoService;
