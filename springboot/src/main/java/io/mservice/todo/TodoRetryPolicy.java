a824088b111a19ede71001e0af3c279a25d723a8
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2024-02-26 09:40:11 +0530

    [update] app property info
    Signed-off-by: Srinivasa Vasu <srinivasan.surprise@gmail.com>

--- springboot/src/main/java/io/mservice/todo/TodoRetryPolicy.java
+++ springboot/src/main/java/io/mservice/todo/TodoRetryPolicy.java
@@ -17,10 +17,13 @@ public class TodoRetryPolicy extends ExceptionClassifierRetryPolicy {
 
 	// 40001 - optimistic concurrency or serialization_failure
 	// 40P01 - deadlock
-	// 08006 - connection issues
+	// 08006 - connection issues (error sending data back)
 	// 57P01 - broken pool conn (invalidated connections because of node failure, etc.)
 	// XX000 - RPC failures (could be intermittent)
 	private final String SQL_STATE = "^(40001)|(40P01)|(57P01)|(08006)|(XX000)";
+
+	// oom-killer or a process crash in the middle of a tx
+	private final String SQL_MSG = "^(connection is closed)";
 
 	private final RetryPolicy sp = new SimpleRetryPolicy(3);
 
@@ -30,13 +33,16 @@ public class TodoRetryPolicy extends ExceptionClassifierRetryPolicy {
 	public void init() {
 		this.setExceptionClassifier(cause -> {
 			do {
+				// SQLTransientConnectionException - connection is refused; intermittent issues because of a backend failure
 				if (cause instanceof SQLRecoverableException || cause instanceof SQLTransientConnectionException) {
 					return sp;
 				}
-				else if (cause instanceof SQLException exception
-						&& (exception.getSQLState() != null && exception.getSQLState().matches(SQL_STATE))) {
+				else if (cause instanceof SQLException exception) {
+						if ((exception.getSQLState() != null && exception.getSQLState().matches(SQL_STATE)) ||
+								(exception.getMessage() != null && exception.getMessage().toLowerCase().matches(SQL_MSG))) {
 					return sp;
 				}
+						}
 				cause = cause.getCause();
 			}
 			while (cause != null);
