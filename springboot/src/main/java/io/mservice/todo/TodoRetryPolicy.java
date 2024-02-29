27a7d5e9107c9d03a501f0ff401290bfa4a2961c
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2024-02-29 08:49:05 +0530

    [update] conn props
    Signed-off-by: Srinivasa Vasu <srinivasan.surprise@gmail.com>

--- springboot/src/main/java/io/mservice/todo/TodoRetryPolicy.java
+++ springboot/src/main/java/io/mservice/todo/TodoRetryPolicy.java
@@ -17,15 +17,15 @@ public class TodoRetryPolicy extends ExceptionClassifierRetryPolicy {
 
 	// 40001 - optimistic concurrency or serialization_failure
 	// 40P01 - deadlock
-	// 08006 - connection issues (error sending data back)
-	// 57P01 - broken pool conn (invalidated connections because of node failure, etc.)
+	// 08006 - connection issues (error sending data back);"kill -9" failures, socket timeouts
+	// 57P01 - broken pool conn (invalidated connections because of node-failure, node-restart, etc.);"kill -15" failures
 	// XX000 - RPC failures (could be intermittent)
 	private final String SQL_STATE = "^(40001)|(40P01)|(57P01)|(08006)|(XX000)";
 
 	// oom-killer or a process crash in the middle of a tx
 	private final String SQL_MSG = "^(connection is closed)";
 
-	private final RetryPolicy sp = new SimpleRetryPolicy(3);
+	private final RetryPolicy sp = new SimpleRetryPolicy(5);
 
 	private final RetryPolicy np = new NeverRetryPolicy();
 
@@ -33,16 +33,20 @@ public class TodoRetryPolicy extends ExceptionClassifierRetryPolicy {
 	public void init() {
 		this.setExceptionClassifier(cause -> {
 			do {
-				// SQLTransientConnectionException - connection is refused; intermittent issues because of a backend failure
+				// SQLTransientConnectionException: (08001)|(08003)
+				// - intermittent issues because of a backend failure like connection refused
+				// - hikari connection time out
+				// - 08001, 08003 - connection does not exist (pool connection timeout)
 				if (cause instanceof SQLRecoverableException || cause instanceof SQLTransientConnectionException) {
 					return sp;
 				}
 				else if (cause instanceof SQLException exception) {
-						if ((exception.getSQLState() != null && exception.getSQLState().matches(SQL_STATE)) ||
-								(exception.getMessage() != null && exception.getMessage().toLowerCase().matches(SQL_MSG))) {
-					return sp;
+					if ((exception.getSQLState() != null && exception.getSQLState().matches(SQL_STATE))
+							|| (exception.getMessage() != null
+									&& exception.getMessage().toLowerCase().matches(SQL_MSG))) {
+						return sp;
+					}
 				}
-						}
 				cause = cause.getCause();
 			}
 			while (cause != null);
